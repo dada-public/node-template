ee3415a3d416b49a34d8c4695a5d69019c44982d
victor <viktorsantiagogonzalez@gmail.com>
2022-11-26 21:14:49 +0000

    test

--- .github/actions/calculate-version/index.js
+++ .github/actions/calculate-version/index.js
@@ -2,6 +2,14 @@ const github = require("@actions/github");
 const semver = require("semver");
 const core = require("@actions/core");
 
+const getBranchName = (context) => {
+  const {ref} = context;
+  return ref.split("/").slice(-1);
+
+}
+
 core.startGroup("LOG")
 console.log(github);
 core.endGroup();
+
+console.log(getBranchName(github.context))
