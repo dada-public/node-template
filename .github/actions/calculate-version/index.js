ff0bf1a7feae0602fbbaed3cb5610631d3beab4f
victor <viktorsantiagogonzalez@gmail.com>
2022-11-26 22:45:09 +0000

    test

--- .github/actions/calculate-version/index.js
+++ .github/actions/calculate-version/index.js
@@ -1,23 +1,6 @@
 const github = require("@actions/github");
-const core   = require("@actions/core");
 const { run } = require("./src/utils");
 
-( async () => {
-
-  const token = core.getInput("token");
-  const toolkit = github.getOctokit(token);
-  const { context } = github;
-
-  const patterns = {
-    major: core.getInput("major-pattern"),
-    minor: core.getInput("minor-pattern"),
-    patch: core.getInput("patch-pattern")
-  }
-
-  const nextVersion = await run(context, toolkit, patterns);
-
-  core.setOutput(nextVersion);
-
-  console.log(nextVersion);
-
+(async () => {
+  await run();
 })()
--- .github/actions/calculate-version/src/utils.js
+++ .github/actions/calculate-version/src/utils.js
@@ -1,3 +1,5 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
 const semver = require("semver");
 
 const getBranchName = (context) => {
@@ -28,10 +30,26 @@ const calculateNextVersion = (current, branch, patterns) => {
   return !current ? "0.0.0" : semver.inc(current, action);
 }
 
-const run = async (context, toolkit, patterns) => {
+
+
+
+const run = async () => {
+
+  const token = core.getInput("token");
+  const toolkit = github.getOctokit(token);
+  const { context } = github;
+
+  const patterns = {
+    major: core.getInput("major-pattern"),
+    minor: core.getInput("minor-pattern"),
+    patch: core.getInput("patch-pattern")
+  }
+
   const branchName = await getBranchName(context);
   const currentRelease = await getCurrentReleaseTag(toolkit, context);
-  return calculateNextVersion(currentRelease, branchName, patterns);
+  const nextVersion = calculateNextVersion(currentRelease, branchName, patterns);
+
+  core.setOutput("semver", nextVersion);
 }
 
 module.exports = {
