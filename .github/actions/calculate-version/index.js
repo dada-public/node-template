b48ea740f3ac1a2adb86808f065a5d2b60b14dfa
victor <viktorsantiagogonzalez@gmail.com>
2022-11-26 21:03:38 +0000

    test

--- .github/actions/calculate-version/index.js
+++ .github/actions/calculate-version/index.js
@@ -1,3 +1,8 @@
 const github = require("@actions/github");
+const semver = require("semver");
 
 console.log(github.ref);
+
+console.log(`semver MAJOR: ${semver.inc('1.2.3', 'major')}`);
+console.log(`semver MINOR: ${semver.inc('1.2.3', 'minor')}`);
+console.log(`semver PATCH: ${semver.inc('1.2.3', 'patch')}`);
